9f95fc30f671da2daca3da16d13c63c26b3a6e0b
ChangheeKim <galahad0001@ucware.net>
2024-11-02 14:25:20 +0900

    ADD: Docker-Volume script

---
+++ IDE/docker-volume.sh
@@ -0,0 +1,15 @@
+# 볼륨 새성 (volume name : cloud-cicd-src)
+docker volume create  \
+--opt device="/Users/changhee/goodee-cicd/src" \
+--opt o=bind --opt type=none cloud-cicd-src
+
+docker volume create  \
+--opt device="/Users/changhee/goodee-cicd/vscode" \
+--opt o=bind --opt type=none vscode
+
+docker volume create  \
+--opt device="/Users/changhee/goodee-cicd/jenkins" \
+--opt o=bind --opt type=none jenkins-home
+
+# 볼륨 생성 확인
+docker volume ls
